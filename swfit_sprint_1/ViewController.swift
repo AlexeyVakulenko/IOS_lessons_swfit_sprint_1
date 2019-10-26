d35322f42f9278c18260434738d4d8420cfa479e
Aleksey Vakulenko <vakulenko.a.vla@sberbank.ru>
2019-10-27 00:11:29 +0300

    Initial Commit

---
+++ swfit_sprint_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swfit_sprint_1
+//
+//  Created by Aleksey Vakulenko on 27/10/2019.
+//  Copyright © 2019 Aleksey Vakulenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ swfit_sprint_1Tests/swfit_sprint_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  swfit_sprint_1Tests.swift
+//  swfit_sprint_1Tests
+//
+//  Created by Aleksey Vakulenko on 27/10/2019.
+//  Copyright © 2019 Aleksey Vakulenko. All rights reserved.
+//
+
+import XCTest
+@testable import swfit_sprint_1
+
+class swfit_sprint_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
